cc47a1dca3e4bee54a90b35e96eaa160921ea980
marinaRYA <marina.mi.04@mail.ru>
2024-03-22 14:11:33 +0700

    commit

--- cryptosyte/crypto/models.py
+++ cryptosyte/crypto/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Crypto(models.Model):
+  name = models.CharField(max_length=10)
+  price = models.FloatField()
+  time_lastupdate = models.DateTimeField(auto_now=True)
